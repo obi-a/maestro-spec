c6f0999c675b109cc98f453069f24789ef6ccbf3
Obi Akubue <obioraakubue@yahoo.com>
2015-09-14 22:28:53 -0400

    better error reporting c-lang style

--- sample.rb
+++ sample.rb
@@ -2,14 +2,39 @@ require 'treetop'
 
 Treetop.load 'maestro'
 
-parser = MaestroParser.new
+#parser = MaestroParser.new
 
 actions = <<-eos
   exists do
+    h1
     div
   end
 eos
 
-syntax_tree = parser.parse( actions.gsub(/\n|\t/, "") )
+#actions = "  exists do div end"
 
-puts syntax_tree.inspect
+#actions = "h1"
+
+#syntax_tree = parser.parse( actions.gsub(/\n|\t/, "") )
+
+#puts syntax_tree.inspect
+
+def parse(data)
+  if data.respond_to? :read
+    data = data.read
+  end
+
+  parser = MaestroParser.new
+  ast = parser.parse data
+
+  if ast
+    ast.do_something_useful
+  else
+    parser.failure_reason =~ /^(Expected .+) after/m
+    puts "#{$1.gsub("\n", '$NEWLINE')}:"
+    puts data.lines.to_a[parser.failure_line - 1]
+    puts "#{'~' * (parser.failure_column - 1)}^"
+  end
+end
+
+parse(actions)
